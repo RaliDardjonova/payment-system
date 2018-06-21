ef953aa3deda923a94da545ea1751f812874d16a
Ralitsa Dardjonova <r.dardjonova@gmail.com>
2018-06-21 22:44:38 +0300

    add queries to extract name and addres of bank

--- InternationalBank.cpp
+++ InternationalBank.cpp
@@ -1,10 +1,61 @@
 #include "InternationalBank.h"
+#define SWIFT_LENGTH 8
+
+InternationalBank::InternationalBank()
+{
+    bankSWIFT = "";
+}
+
+InternationalBank::InternationalBank(std::string newBankSWIFT)
+{
+    setBankSWIFT(newBankSWIFT);
+}
+
 
 std::string InternationalBank::getBankName() {
-    return "";
+    sql::Statement *statement;
+    sql::ResultSet *result;
+    std::string bankName = "";
+
+    try {
+        statement = connection->createStatement();
+        result = statement->executeQuery("select BankName from InternationalBank where bankSWIFT = '" + this->getBankSWIFT() + "';");
+
+        if(result->next()){
+            bankName = result->getString("bankName");
+        }
+    } catch (sql::SQLException &e) {
+        printSQLError(e);
+    }
+    return bankName;
 }
 
 
 std::string InternationalBank::getBankAddress() {
-    return "";
+    sql::Statement *statement;
+    sql::ResultSet *result;
+    std::string bankAddress = "";
+    std::string query = "select BankAddress from InternationalBank where bankSWIFT = '" + this->getBankSWIFT() + "';";
+
+    try {
+        statement = connection->createStatement();
+        result = statement->executeQuery(query);
+
+        if(result->next()){
+            bankAddress = result->getString("bankAddress");
+        }
+    } catch (sql::SQLException &e) {
+        printSQLError(e);
+    }
+    return bankAddress;
+}
+
+std::string InternationalBank::getBankSWIFT() {
+    return bankSWIFT;
+}
+
+void InternationalBank::setBankSWIFT(std::string newBankSWIFT){
+    if(newBankSWIFT.size() == SWIFT_LENGTH){
+        bankSWIFT = newBankSWIFT;
+    }
 }
--- InternationalBank.h
+++ InternationalBank.h
@@ -1,11 +1,16 @@
 #ifndef _INTERNATIONALBANK_H
 #define _INTERNATIONALBANK_H
 #include<string>
+#include "sql-controllers.h"
 
 class InternationalBank {
 public:
+    InternationalBank();
+    InternationalBank(std::string bankSWIFT);
     std::string getBankName();
     std::string getBankAddress();
+    std::string getBankSWIFT();
+    void setBankSWIFT(std::string bankSWIFT);
 private:
     std::string bankSWIFT;
 };
