af9d21274d174314bb1f058595ed3d7a8f007add
Ralitsa Dardjonova <r.dardjonova@gmail.com>
2018-06-23 03:39:48 +0300

    Add class to get number in words

---
+++ NumberInWords.h
@@ -0,0 +1,28 @@
+#ifndef _NUMBER_IN_WORDS
+#define _NUMBER_IN_WORDS
+#include<vector>
+#include<string>
+
+class NumberInWords{
+public:
+    NumberInWords();
+    NumberInWords(const NumberInWords&);
+    NumberInWords(long long, unsigned int);
+    ~NumberInWords();
+    void setWholePart(long long);
+    void setDecimalPart(unsigned int);
+
+    long long getWholePart() const;
+    int getDecimalPart() const;
+    std::string getWholePartInWords();
+    std::string getDecimalPartInWords();
+private:
+    long long wholePart;
+    unsigned int decimalPart;
+    static std::vector<std::string> ones;
+    static std::vector<std::string> teens;
+    static std::vector<std::string> tens;
+    static std::string nameForNumber(long long);
+};
+
+#endif // _NUMBER_IN_WORDS
